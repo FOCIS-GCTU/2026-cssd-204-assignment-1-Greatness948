4ab37d9405fba3d4219bc146092fb4238e7fce55
Greatness948 <ohenemog@gmail.com>
2026-05-31 03:07:57 +0000

    Add files via upload

---
+++ initialss.py
@@ -0,0 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Ohene Ntim
+# STUDENT ID: 2425402378
+# Email: 2425402378@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Ohene Ntim, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    print()
+    print("...ONO")
+    print()
+    print("... OOOOOOOOOO     ...NN        NN    ... OOOOOOOOOO     ...")
+    print("...OO        OO    ...NNN       NN    ...OO        OO    ...")
+    print("...OO        OO    ...NNNN      NN    ...OO        OO    ...")
+    print("...OO        OO    ...NN NN     NN    ...OO        OO    ...")
+    print("...OO        OO    ...NN  NN    NN    ...OO        OO    ...")
+    print("...OO        OO    ...NN    NN  NN    ...OO        OO    ...")
+    print("...OO        OO    ...NN     NN NN    ...OO        OO    ...")
+    print("...OO        OO    ...NN      NNNN    ...OO        OO    ...")
+    print("...OO        OO****...NN       NNN****...OO        OO****...")
+    print("... OOOOOOOOOO ****...NN        NN****... OOOOOOOOOO ****...")
+    print()
+
+
+main()
